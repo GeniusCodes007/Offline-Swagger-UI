77247b039072ffe55979aa2ae7fd667a924c8366
GeniusCodes007 <geniuscodes007@gmail.com>
2026-04-28 20:31:16 +0100

    Adding Method 2 of Swagger UI Offline Display

--- setting_up_swagger_offline.py
+++ setting_up_swagger_offline.py
@@ -1,4 +1,7 @@
 """
+
+METHOD 1:
+
 First:Download the zip file containing the swagger ui files from https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip or
 
     Download the one provided here
@@ -11,17 +14,17 @@ Fourth: Create a Python file 'dump_spec.py' in your project folder
 
 Fifth: Input the code:
 
-        import json
+import json
 
-        from main import app # replace 'main' with name of the Python file containing your application, and
+from main import app # replace 'main' with name of the Python file containing your application, and
 
-        # 'app' with the name of the application instance
+# 'app' with the name of the application instance
 
-        with open('docs/openapi.json', 'w') as json_file: # You replace 'json_file' with any string of your choice
+with open('docs/openapi.json', 'w') as json_file: # You replace 'json_file' with any string of your choice
 
-            json.dump(app.openapi(), json_file, indent=2)
+    json.dump(app.openapi(), json_file, indent=2)
 
-        print('Created openapi.json in your project folder')
+print('Created openapi.json in your project folder')
 
 Sixth: Run the command:
 
@@ -29,6 +32,35 @@ Sixth: Run the command:
 
 Seventh: You're done and good to go!
 
+
+
+METHOD 2
+
+First:Download the zip file containing the swagger ui files from https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip or
+
+    Download the one provided here
+    
+Second: Create a directory 'static' in your project folder
+
+Second: Extract or Unzip the contents of the zip file into your project folder
+
+Third: Copy the directory 'dist' from the unzipped file into 'static', and rename if you wish
+
+Fourth: In the file containing your FastAPI app instance, do put the following code:
+
+
+app_instance_name=FastAPI(
+                # Disable the urls for both docs and redoc
+                docs_url=None,
+                redoc_url=None,
+                   )    
+
+# Create url to display the Swagger UI documentation
+@shop_app.get("/docs", include_in_schema=False)
+def docs():
+    # Import the RedirectResponse class to refer Swagger UI to another page for the documentation
+    from fastapi.responses import RedirectResponse
+    return RedirectResponse(url="/static/my_swagger/index.html")
 
 
 """
